1b66a0af689b5c390eac3740c19cffb82072365e
arbabiiman <arbabiiman@yahoo.com>
2021-09-01 09:58:36 +0430

    add staticroot with collectstatic

--- yas7/settings.py
+++ yas7/settings.py
@@ -134,6 +134,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'media/')
 ]
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root/')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
