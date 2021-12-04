85b0d20181e60035f6942ddf1d37b5157c6ba5d8
arbabiiman <arbabiiman@yahoo.com>
2021-12-04 19:16:53 +0330

    django_cleanup install for cleanup media files

--- yas7/settings.py
+++ yas7/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'accounts.apps.AccountsConfig',
     'teachme.apps.TeachmeConfig',
     'blog.apps.ClerkConfig',
+    'django_cleanup.apps.CleanupConfig',
 ]
 
 MIDDLEWARE = [
