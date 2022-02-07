3465b67764c04882bf5f4aa5eb4db00f7bc00ec1
iman <arbabiiman@yahoo.com>
2022-02-07 19:28:58 +0330

    student submit deactivated

--- teachme/urls.py
+++ teachme/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     # path('',views.index,name='teachme_index'),
     path('teacher/list', views.teacher_list, name="teacher_list"),
     path('teacher/detail/<int:teacher_id>', views.teacher_detail, name="teacher_detail"),
-    path('teacher/request', views.teacher_request, name="teacher_request"),
+    path('teacher/request/<int:teacher_id>', views.teacher_request, name="teacher_request"),
     # path('message/viewer/<str:message_get>', views.message_viewer, name='message_viewer'),
 ]
--- teachme/views.py
+++ teachme/views.py
@@ -12,46 +12,47 @@ from accounts.models import  Teacher, MyUser, StudentSubmit
 from teachme.send_sms import *
 
 
-@login_required
+
 def teacher_detail(request, teacher_id):
-    teacher_selected = None
-    student_user_id = None
-    if hasattr(request.user, 'studentsubmit') ==False and  hasattr(request.user, 'teacher') == False:
+    # teacher_selected = None
+    # student_user_id = None
+    # if hasattr(request.user, 'studentsubmit') ==False and  hasattr(request.user, 'teacher') == False:
+    # if request.user.is_anonymous:
+    #     return render(request,'accounts/login.html',context={'teacher_id':teacher_id})
 
-        return render(request,'accounts/student_submit.html',context={'teacher_id':teacher_id})
 
-
-    student_user_id = request.user.id
+    # student_user_id = request.user.id
     teacher_selected = Teacher.objects.get(pk=teacher_id)
-    student_category = teacher_selected.category_id
+    # student_category = teacher_selected.category_id
 
     #     return HttpResponseRedirect(reverse('teachme:message_viewer',args=['شما به عنوان معلم ثبت شده اید برای درخواست استاد دوباره با شماره تلفن و ایمیل جدید به عنوان دانش آموز ثبت نام کنید']))
 
     context = {
         'teacher_selected': teacher_selected,
-        'student_user_id': student_user_id,
-        'student_category': student_category,
+        # 'student_user_id': student_user_id,
+        # 'student_category': student_category,
     }
     return render(request, 'teachme/teacher_detail.html', context)
 
 @login_required
-def teacher_request(request):
+def teacher_request(request,teacher_id):
     clerk_phone = '09361164819'
-    teacher_id = request.POST.get('teacher_selected_id')
+
     teacher_requested = Teacher.objects.get(id=teacher_id)
     teacher_requested_user_id = getattr(teacher_requested,'user_id')
     teacher_requested_user_params = MyUser.objects.get(id=teacher_requested_user_id)
     teacher_phone = teacher_requested_user_params.phone_number
-    student_user_id = request.POST.get('student_user_id')
-    if hasattr(request.user, 'studentsubmit'):
-        student = StudentSubmit.objects.get(user_id=student_user_id)
-    if hasattr(request.user, 'teacher'):
-        student = Teacher.objects.get(user_id=student_user_id)
-    student_user_params = MyUser.objects.get(id=student_user_id)
-    student_phone = student_user_params.phone_number
+
+    student = MyUser.objects.get(pk=request.user.id)
+    # if hasattr(request.user, 'studentsubmit'):
+    #     student = StudentSubmit.objects.get(user_id=student_user_id)
+    # if hasattr(request.user, 'teacher'):
+    #     student = Teacher.objects.get(user_id=student_user_id)
+    # student_user_params = MyUser.objects.get(id=student_user_id)
+    student_phone = student.phone_number
     clerk_sms_token = 'id:{},uid{}'.format(teacher_id, teacher_requested_user_id)
     clerk_sms_token2 = teacher_phone
-    clerk_sms_token3 = 'id{},{}'.format(student.id,student_phone)
+    clerk_sms_token3 = 'uid{},{}'.format(student.id,student_phone)
     send_sms_stu(student_phone,teacher_requested.last_name)
     send_sms_clerk(clerk_phone,clerk_sms_token,clerk_sms_token2,clerk_sms_token3)
     message = 'درخواست شما ثبت شد بزودی جهت هماهنگی با شما تماس می گیریم'
