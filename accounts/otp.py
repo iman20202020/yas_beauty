fa6b49c0a43c2d5bd42786203ab7d2f84ca105d2
iman <arbabiiman@yahoo.com>
2023-02-10 10:58:34 +0330

    22bahman1401

--- accounts/otp.py
+++ accounts/otp.py
@@ -35,6 +35,7 @@ def send_otp(mobile_number):
 
 def send_sms_teacher_edit(mobile_number,token,token2,token3):
 
+
     try:
         api = KavenegarAPI('4C446667424F455A36304C484E7A4B466633597532372F594D4C514F3356457162524C793056523168626F3D',)
         params = {
