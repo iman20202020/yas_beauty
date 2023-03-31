aa415652f303aa0e98ff5eec354e6db726be1877
iman <iarbabi2020@gmail.com>
2023-03-31 14:14:27 +0430

    before change css ,js,h1

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,7 @@ from blog import views
 app_name = 'blog'
 
 urlpatterns = [
-    path('<str:name>', views.blog_view, name='blog_view'),
+    path('', views.blog_view, name='blog_view'),
+    path('<str:name>', views.named_blog_view, name='named_blog_view')
 
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
+from django.urls import reverse
 
 
-def blog_view(request, name):
+def named_blog_view(request, name):
     template_name = f'blog/{name}.html'
     context = {'name': name}
     return render(request, template_name, context)
 
 
-
+def blog_view(request):
+    return render(request,'blog/blogs.html', {})
