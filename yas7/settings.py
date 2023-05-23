57d183548e53985c0a87febf6a0fd377606ada4d
imanarbabi <iarbabi2020@gmail.com>
2023-05-23 17:11:50 +0330

    Update1 settings.py for deploy

--- yas7/settings.py
+++ yas7/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 DEBUG = True
 
 
-ALLOWED_HOSTS =[]
+ALLOWED_HOSTS =['yasacademy.ir', 'www.yasacademy.ir']
 
 
 # Application definition
